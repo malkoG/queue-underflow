ac9e5dc07855cd37c080f9856ba23b459bac125e
malkoG <rijgndqw012@gmail.com>
2017-07-16 18:59:33 +0900

    Try SPA

---
+++ web/static/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Header extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="Header">
+                <p>Hell-o-world</p>
+            </div>
+        );
+    }
+}
